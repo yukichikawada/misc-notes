c133eda3b655175c11c42a7a230506982290a558
Yukichi Kawada <yukichikawada@gmail.com>
2018-09-10 11:58:59 -0700

    Add Time to log of heroku pings

--- heroku_ping.rb
+++ heroku_ping.rb
@@ -3,12 +3,14 @@ require 'net/ping'
 
 loop do
   smedium = Net::HTTP.new('yk-smedium.herokuapp.com')
+  smedium_time = Time.now
   response = smedium.request_get('/')
-  p "smedium response: " + response.code
+  p "smedium response: " + response.code + " ~ #{smedium_time}"
 
   evictions = Net::HTTP.new('yk-sf-evictions-analysis.herokuapp.com')
+  eviction_time = Time.now
   response = evictions.request_get('/')
-  p "evictions response: " + response.code
+  p "evictions response: " + response.code + " ~ #{eviction_time}"
 
   # ping heroku dynos every 30 min to keep awake
   sleep 3000
