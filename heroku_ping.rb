72d190fe05d2edf52a5318bdef1eec004dd6f608
Yukichi Kawada <yukichikawada@gmail.com>
2018-09-11 20:54:27 -0700

    Refactor ping log for terminal readability

--- heroku_ping.rb
+++ heroku_ping.rb
@@ -5,12 +5,12 @@ loop do
   smedium = Net::HTTP.new('yk-smedium.herokuapp.com')
   smedium_time = Time.now
   response = smedium.request_get('/')
-  p "smedium response: " + response.code + " ~ #{smedium_time}"
+  p "#{smedium_time} ~ " + "smedium response: " + response.code 
 
   evictions = Net::HTTP.new('yk-sf-evictions-analysis.herokuapp.com')
   eviction_time = Time.now
   response = evictions.request_get('/')
-  p "evictions response: " + response.code + " ~ #{eviction_time}"
+  p "#{eviction_time} ~ " + "evictions response: " + response.code 
 
   # ping heroku dynos every 30 min to keep awake
   sleep (60 * 30)
